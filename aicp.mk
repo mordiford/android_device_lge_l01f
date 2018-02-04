9f3397ba1523efe0cc97139060b631b914e79e44
lindwurm <lindwurm.q@gmail.com>
2018-02-04 17:47:44 +0900

    AICP-ify
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- aicp.mk
+++ aicp.mk
@@ -1,12 +1,12 @@
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/l01f/l01f.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := l01f
-PRODUCT_NAME := lineage_l01f
+PRODUCT_NAME := aicp_l01f
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := L-01F
 PRODUCT_MANUFACTURER := lge
@@ -14,3 +14,11 @@ PRODUCT_MANUFACTURER := lge
 PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT=lge/g2_dcm_jp/g2:4.2.2/JDQ39B/L01F11k.1386142206:user/release-keys \
     PRIVATE_BUILD_DESC="g2_dcm_jp-user 4.2.2 JDQ39B L01F11k.1386142206 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        DEVICE_MAINTAINERS=""
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
